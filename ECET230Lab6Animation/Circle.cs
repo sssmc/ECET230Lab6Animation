2460a36438179893f1e7376684b74ed0e57fca35
sebastien <sebastien@robitaille.info>
2023-10-10 12:47:01 -0700

    Added basic Circle class and basic test program

---
+++ ECET230Lab6Animation/Circle.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Meadow.Foundation.Graphics;
+using Meadow.Foundation;
+
+namespace ECET230Lab6Animation
+{
+	public class Circle
+	{
+		private MicroGraphics graphics;
+		private int displayWidth;
+		private int displayHeight;
+
+		private int radius;
+		private int positionX;
+		private int positionY;
+
+		private bool filled;
+
+		private int speedX;
+		private int speedY;
+
+		private Color ballColor;
+
+		public Circle(MicroGraphics graphics, int displayWidth, int displayHeight, Color ballColor, bool filled)
+		{
+			this.graphics = graphics;
+			this.displayWidth = displayWidth;
+			this.displayHeight = displayHeight;
+			this.filled = filled;
+			this.ballColor = ballColor;
+			positionX = 0;
+			positionY = 0;
+		}
+
+		public void setGraphics(MicroGraphics graphics)
+		{
+			this.graphics = graphics;
+		}
+
+		public void setDisplaySize(int width, int height)
+		{
+			displayHeight = height;
+			displayWidth = width;
+		}
+
+		public int getPositionY()
+		{
+			return positionY;
+
+		}
+		public int getPositionX()
+		{
+			return positionX;
+		}
+
+		public void setPosition(int x, int y)
+		{
+			positionX = x;
+			positionY = y;
+		}
+
+		public int getRadius()
+		{
+			return radius;
+		}
+
+		public void setRadius(int radius)
+		{
+			this.radius = radius;
+		}
+
+		public void setFilled(bool filled)
+		{
+			this.filled = filled;
+		}
+
+		public Color getBallColor()
+		{
+			return ballColor;
+		}
+		public void setBallColor(Color ballColor)
+		{
+			this.ballColor = ballColor;
+		}
+
+		public void draw()
+		{
+            graphics.DrawCircle
+            (
+                centerX: positionX,
+                centerY: positionY,
+                radius: radius,
+                color: ballColor,
+				filled: filled
+            );
+        }
+	}
+}
+
--- ECET230Lab6Animation/MeadowApp.cs
+++ ECET230Lab6Animation/MeadowApp.cs
@@ -1,63 +1,78 @@
 ﻿using Meadow;
 using Meadow.Devices;
 using Meadow.Foundation;
+using Meadow.Foundation.Displays;
+using Meadow.Foundation.Graphics;
 using Meadow.Foundation.Leds;
-using Meadow.Peripherals.Leds;
 using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ECET230Lab6Animation
 {
-    // Change F7FeatherV2 to F7FeatherV1 for V1.x boards
+    // public class MeadowApp : App<F7FeatherV1> <- If you have a Meadow F7v1.*
     public class MeadowApp : App<F7FeatherV2>
     {
-        RgbPwmLed onboardLed;
+        readonly Color WatchBackgroundColor = Color.White;
+
+        MicroGraphics graphics;
+        int displayWidth, displayHeight;
+        int hour, minute, tick;
 
         public override Task Initialize()
         {
-            Resolver.Log.Info("Initialize...");
-
-            onboardLed = new RgbPwmLed(
+            var onboardLed = new RgbPwmLed(
                 redPwmPin: Device.Pins.OnboardLedRed,
                 greenPwmPin: Device.Pins.OnboardLedGreen,
-                bluePwmPin: Device.Pins.OnboardLedBlue,
-                CommonType.CommonAnode);
+                bluePwmPin: Device.Pins.OnboardLedBlue);
+            onboardLed.SetColor(Color.Red);
+
+            var st7789 = new St7789
+            (
+                spiBus: Device.CreateSpiBus(),
+                chipSelectPin: Device.Pins.D02,
+                dcPin: Device.Pins.D01,
+                resetPin: Device.Pins.D00,
+                width: 240, height: 240
+            );
+            displayWidth = Convert.ToInt32(st7789.Width);
+            displayHeight = Convert.ToInt32(st7789.Height);
+
+            graphics = new MicroGraphics(st7789);
+            graphics.Rotation = RotationType._270Degrees;
+
+            onboardLed.SetColor(Color.Green);
+
+            graphics.Clear(updateDisplay: true);
 
             return base.Initialize();
         }
 
         public override Task Run()
         {
-            Resolver.Log.Info("Run...");
-
-            return CycleColors(TimeSpan.FromMilliseconds(1000));
-        }
-
-        async Task CycleColors(TimeSpan duration)
-        {
-            Resolver.Log.Info("Cycle colors...");
 
             while (true)
             {
-                await ShowColorPulse(Color.Blue, duration);
-                await ShowColorPulse(Color.Cyan, duration);
-                await ShowColorPulse(Color.Green, duration);
-                await ShowColorPulse(Color.GreenYellow, duration);
-                await ShowColorPulse(Color.Yellow, duration);
-                await ShowColorPulse(Color.Orange, duration);
-                await ShowColorPulse(Color.OrangeRed, duration);
-                await ShowColorPulse(Color.Red, duration);
-                await ShowColorPulse(Color.MediumVioletRed, duration);
-                await ShowColorPulse(Color.Purple, duration);
-                await ShowColorPulse(Color.Magenta, duration);
-                await ShowColorPulse(Color.Pink, duration);
-            }
-        }
+                
 
-        async Task ShowColorPulse(Color color, TimeSpan duration)
-        {
-            await onboardLed.StartPulse(color, duration / 2);
-            await Task.Delay(duration);
+                Random random = new Random();
+
+                Color randomColor = Color.FromRgb(random.Next(255), random.Next(255), random.Next(255));
+
+                Circle circle = new Circle(graphics, 240, 240, randomColor, false); ;
+
+                circle.setPosition(random.Next(240), random.Next(240));
+                circle.setRadius(random.Next(3, 40));
+
+                circle.draw();
+
+                graphics.Show();
+
+                Thread.Sleep(500);
+            }
+
+
+            return base.Run();
         }
     }
 }
