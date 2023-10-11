22325289b511e9d26c6b08cee9045de7be94c42d
sebastien <sebastien@robitaille.info>
2023-10-11 13:19:45 -0700

    Comments and changed fileds to props with get set

--- ECET230Lab6Animation/Circle.cs
+++ ECET230Lab6Animation/Circle.cs
@@ -6,98 +6,51 @@ namespace ECET230Lab6Animation
 {
 	public class Circle
 	{
-		private MicroGraphics graphics;
-		private int displayWidth;
-		private int displayHeight;
 
-		private int radius;
-		private int positionX;
-		private int positionY;
+		public readonly MicroGraphics graphics;
 
-		private bool filled;
+		public int displayWidth { get; set; }
+		public int displayHeight { get; set; }
 
-		private int speedX;
-		private int speedY;
+		public int radius { get; set; }
+		public int positionX { get; set; }
+		public int positionY { get; set; }
 
-		private Color ballColor;
+		public bool filled { get; set; }
 
-		public Circle(MicroGraphics graphics, int displayWidth, int displayHeight, Color ballColor, bool filled)
+		public int speedX { get; set; }
+		public int speedY { get; set; }
+
+		public Color color { get; set; }
+
+		public Circle(
+			MicroGraphics graphics,
+			int displayWidth,
+			int displayHeight,
+			Color color,
+			bool filled)
 		{
 			this.graphics = graphics;
 			this.displayWidth = displayWidth;
 			this.displayHeight = displayHeight;
 			this.filled = filled;
-			this.ballColor = ballColor;
+			this.color = color ;
 			positionX = 0;
 			positionY = 0;
 			speedX = 0;
 			speedY = 0;
 		}
 
-		public void setGraphics(MicroGraphics graphics)
-		{
-			this.graphics = graphics;
-		}
-
-		public void setDisplaySize(int width, int height)
-		{
-			displayHeight = height;
-			displayWidth = width;
-		}
-
-		public int getPositionY()
-		{
-			return positionY;
-
-		}
-		public int getPositionX()
-		{
-			return positionX;
-		}
-
-		public void setPosition(int x, int y)
-		{
-			positionX = x;
-			positionY = y;
-		}
-
-		public void setSpeed(int speedX, int speedY)
-		{
-			this.speedX = speedX;
-			this.speedY = speedY;
-		}
-
-		public int getRadius()
-		{
-			return radius;
-		}
-
-		public void setRadius(int radius)
-		{
-			this.radius = radius;
-		}
-
-		public void setFilled(bool filled)
-		{
-			this.filled = filled;
-		}
-
-		public Color getBallColor()
-		{
-			return ballColor;
-		}
-		public void setBallColor(Color ballColor)
-		{
-			this.ballColor = ballColor;
-		}
-
 		private void updatePosition()
 		{
+			//Move the circle in x and y depending on the x and y speeds
 			positionX += speedX;
 			positionY += speedY;
 
+			//Check if the circle is outside the display
 			if((positionX >= (displayWidth - radius)) || (positionX <= (0 + radius)))
 			{
+				//Reverse the speed
 				speedX *= -1;
 
 			}
@@ -110,14 +63,16 @@ namespace ECET230Lab6Animation
 
 		public void draw()
 		{
+			//Update the circle's position
 			updatePosition();
 
+			//Draw the Circle
             graphics.DrawCircle
             (
                 centerX: positionX,
                 centerY: positionY,
                 radius: radius,
-                color: ballColor,
+                color: color,
 				filled: filled
             );
         }
